69f0a964fd3c526c7d13e59976627f3b76666b5f
firstname lastname <email@domain>
2013-03-20 07:56:34 -0500

    remove intersphinx reference to docs.python.org in conf.py

--- conf.py
+++ conf.py
@@ -283,7 +283,3 @@ epub_copyright = u'2012, Gerald Lovel'
 
 # Allow duplicate toc entries.
 #epub_tocdup = True
-
-
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
